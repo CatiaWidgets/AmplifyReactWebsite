8fc9e2c78bfa4651d55fc5dbd11fc80e839641dc
LekoLabs <48772575+LekoLabs@users.noreply.github.com>
2021-04-05 16:38:38 -0700

    q

--- amplifycatiawidgets/src/App.js
+++ amplifycatiawidgets/src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
               {backdrop}
               
               <main style={{marginTop:'64px'} }>
-                  <p>This is the page content!</p>
+                  <p>This is the page content! added proxy to package.json</p>
                   <TestRest />
               </main>
         </div>
