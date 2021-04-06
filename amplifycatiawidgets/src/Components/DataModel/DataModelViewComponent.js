ecc31021d4332353b6821a31a8b7bf952110bc13
LekoLabs <48772575+LekoLabs@users.noreply.github.com>
2021-04-06 16:33:07 -0700

    h

--- amplifycatiawidgets/src/Components/DataModel/DataModelViewComponent.js
+++ amplifycatiawidgets/src/Components/DataModel/DataModelViewComponent.js
@@ -5,6 +5,7 @@ import React, { useMemo, useState, useEffect } from "react";
 import DataModelComponent from "./DataModelComponent";
 
 function DataModelViewComponent() {
+    const [data, setData] = useState([]);
 
     var axios = require('axios');
 
@@ -19,14 +20,65 @@ function DataModelViewComponent() {
     axios(config)
         .then(function (response) {
             console.log(JSON.stringify(response.data));
+            setData(response.data);
         })
         .catch(function (error) {
             console.log(error);
         });
 
+
+    /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+    const columns = useMemo(
+        () => [
+            {
+                // first group - TV Show
+                Header: "TV Show",
+                // First group columns
+                columns: [
+                    {
+                        Header: "Name",
+                        accessor: "show.name"
+                    },
+                    {
+                        Header: "Type",
+                        accessor: "show.type"
+                    }
+                ]
+            },
+            {
+                // Second group - Details
+                Header: "Details",
+                // Second group columns
+                columns: [
+                    {
+                        Header: "Language",
+                        accessor: "show.language"
+                    },
+                    {
+                        Header: "Genre(s)",
+                        accessor: "show.genres"
+                    },
+                    {
+                        Header: "Runtime",
+                        accessor: "show.runtime"
+                    },
+                    {
+                        Header: "Status",
+                        accessor: "show.status"
+                    }
+                ]
+            }
+        ],
+        []
+    );
+
+
     return (
         <div className="App">
-            
+            <DataModelComponent columns={columns} data={data} />
         </div>
     );
 }
