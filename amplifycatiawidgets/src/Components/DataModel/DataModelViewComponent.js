b022f2b2d09988f9ccfe03a440f960a597a00def
LekoLabs <48772575+LekoLabs@users.noreply.github.com>
2021-04-06 16:44:14 -0700

    t

--- amplifycatiawidgets/src/Components/DataModel/DataModelViewComponent.js
+++ amplifycatiawidgets/src/Components/DataModel/DataModelViewComponent.js
@@ -1,86 +1,54 @@
-//https://blog.logrocket.com/complete-guide-building-smart-data-table-react/
-
 import React, { useMemo, useState, useEffect } from "react";
 
-import DataModelComponent from "./DataModelComponent";
 
-function DataModelViewComponent() {
-    const [data, setData] = useState([]);
-
-    var axios = require('axios');
-
-    var config = {
-        method: 'get',
-        url: 'https://ip2dfwmit2.execute-api.eu-west-1.amazonaws.com/Prod/v1/DataModel',
-        headers: {
-            'x-api-key': 'hygscUxN0S5qg5WzLKjb2FLmSE9NMCF5KOXxO3zf','Access-Control-Allow-Origin':'*'
+export default class DataModelViewComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [],
+            loading: true
         }
-    };
+    }
 
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-            setData(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    async getUsersData() {
 
-
-    /* 
-    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
-    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
-  */
-    const columns = useMemo(
-        () => [
-            {
-                // first group - TV Show
-                Header: "TV Show",
-                // First group columns
-                columns: [
-                    {
-                        Header: "Name",
-                        accessor: "show.name"
-                    },
-                    {
-                        Header: "Type",
-                        accessor: "show.type"
-                    }
-                ]
-            },
-            {
-                // Second group - Details
-                Header: "Details",
-                // Second group columns
-                columns: [
-                    {
-                        Header: "Language",
-                        accessor: "show.language"
-                    },
-                    {
-                        Header: "Genre(s)",
-                        accessor: "show.genres"
-                    },
-                    {
-                        Header: "Runtime",
-                        accessor: "show.runtime"
-                    },
-                    {
-                        Header: "Status",
-                        accessor: "show.status"
-                    }
-                ]
+        var config = {
+            method: 'get',
+            url: 'https://ip2dfwmit2.execute-api.eu-west-1.amazonaws.com/Prod/v1/DataModel',
+            headers: {
+                'x-api-key': 'hygscUxN0S5qg5WzLKjb2FLmSE9NMCF5KOXxO3zf', 'Access-Control-Allow-Origin': '*'
             }
-        ],
-        []
-    );
+        };
 
+        const res = await axios(config)
+        console.log(res.data)
+        this.setState({ loading: false, users: res.data })
+    }
 
-    return (
-        <div className="App">
-            <DataModelComponent columns={columns} data={data} />
-        </div>
-    );
+    componentDidMount() {
+        this.getUsersData()
+    }
+
+    render() {
+        const columns = [{
+            Header: 'firstName',
+            accessor: 'firstName',
+        }
+            , {
+                Header: 'lastName',
+                accessor: 'lastNamee',
+        }
+
+            , {
+                Header: 'age',
+                accessor: 'age',
+        }
+        ]
+        return (
+            <ReactTable
+                data={this.state.users}
+                columns={columns}
+            />
+        )
+    }
 }
-
-export default DataModelViewComponent;
